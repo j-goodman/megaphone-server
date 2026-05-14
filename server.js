8501cc651d3a7fa30a2fba0cd600e92156da9eae
j-goodman <johncurtisgoodman@gmail.com>
2026-05-14 18:49:09 -0400

    Code updates.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const uri = process.env.MONGO_URI
 const express = require("express")
